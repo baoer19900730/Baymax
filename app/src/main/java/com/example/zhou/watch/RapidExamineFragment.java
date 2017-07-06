f082a45102226b4c18a7ea92be21679eb3b11b09
jxr202 <jxr202@foxmail.com>
2017-07-06 12:00:17 +0800

    获取摄像头

--- app/src/main/java/com/example/zhou/watch/RapidExamineFragment.java
+++ app/src/main/java/com/example/zhou/watch/RapidExamineFragment.java
@@ -5,6 +5,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,11 +22,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 /**
  * Created by zhou on 2017/7/5
  */
 
 public class RapidExamineFragment extends Fragment implements SurfaceHolder.Callback{
+
+    private static final String TAG = "RapidExamineFragment";
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
     private   Camera camera;
@@ -34,22 +40,47 @@ public class RapidExamineFragment extends Fragment implements SurfaceHolder.Call
         View view = inflater.inflate(R.layout.rapid_examine, container, false);
         TextView startText = (TextView) view.findViewById(R.id.begin_text);
         surfaceView = (SurfaceView) view.findViewById(R.id.main_surface_view);
+
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);// 设置显示器类型，setType必须设置
+        surfaceHolder.setFormat(PixelFormat.TRANSLUCENT);
+
         startText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                surfaceChanged();
+                startPreview();
             }
         });
         return view;
     }
 
 
+    /**
+     * 调用
+     */
+    public void startPreview() {
+        Log.i(TAG, "startPreview.. ");
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                camera.setPreviewDisplay(surfaceHolder);
+                Camera.Parameters params = camera.getParameters();
+                params.setPreviewSize(352, 288);
+                camera.setParameters(params);
+                camera.startPreview();
+                camera.setDisplayOrientation(90);  //旋转90度
+                //camera.setPreviewCallback(this);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        try {
+        Log.i(TAG, "surfaceCreated..");
+        /*try {
           camera = Camera.open();
             camera.setPreviewDisplay(holder);
         }catch (Exception e){
@@ -58,16 +89,19 @@ public class RapidExamineFragment extends Fragment implements SurfaceHolder.Call
                 camera = null;
             }
             e.printStackTrace();
-        }
+        }*/
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            camera.startPreview();
+            //camera.startPreview();
+        Log.i(TAG, "surfaceChanged..");
+        //startPreview();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.i(TAG, "surfaceDestroyed..");
          if (null != camera){
              camera.stopPreview();
              camera.release();
