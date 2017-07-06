a02c0306de3f1fa76103ede66e213d4faa157a2f
jxr202 <jxr202@foxmail.com>
2017-07-06 12:18:55 +0800

    添加注释

--- app/src/main/java/com/example/zhou/watch/RapidExamineFragment.java
+++ app/src/main/java/com/example/zhou/watch/RapidExamineFragment.java
@@ -39,12 +39,12 @@ public class RapidExamineFragment extends Fragment implements SurfaceHolder.Call
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.rapid_examine, container, false);
         TextView startText = (TextView) view.findViewById(R.id.begin_text);
-        surfaceView = (SurfaceView) view.findViewById(R.id.main_surface_view);
+        surfaceView = (SurfaceView) view.findViewById(R.id.main_surface_view);  //拿到surfaceView对象
 
-        surfaceHolder = surfaceView.getHolder();
-        surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);// 设置显示器类型，setType必须设置
-        surfaceHolder.setFormat(PixelFormat.TRANSLUCENT);
+        surfaceHolder = surfaceView.getHolder();    //SurfaceView在创建的时候，就会自动生成一个Holder，这个对象后期我们有用，所以保存到全局变量
+        surfaceHolder.addCallback(this);            //SurfaceView需要一个回调，用于告诉你，什么时候开始创建，什么时候创建完成，什么时候销毁，也就是下面的那三个方法
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);// 设置显示器类型，setType必须设置   Holder必须要设置成这个类型，才能支持显示摄像头数据
+        surfaceHolder.setFormat(PixelFormat.TRANSLUCENT);   //这一句不知道什么意思
 
         startText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,26 +57,30 @@ public class RapidExamineFragment extends Fragment implements SurfaceHolder.Call
 
 
     /**
-     * 调用
+     * 调用摄像头
      */
     public void startPreview() {
         Log.i(TAG, "startPreview.. ");
         if (camera == null) {
             try {
-                camera = Camera.open();
-                camera.setPreviewDisplay(surfaceHolder);
-                Camera.Parameters params = camera.getParameters();
-                params.setPreviewSize(352, 288);
-                camera.setParameters(params);
-                camera.startPreview();
-                camera.setDisplayOrientation(90);  //旋转90度
-                //camera.setPreviewCallback(this);
+                camera = Camera.open(); //获取Camera对象，思考一下为什么是open而不是new一个Camera，它这里使用了什么样的设计模式
+                camera.setPreviewDisplay(surfaceHolder);    //Camera需要一个Holder对象才能显示，不然它显示在哪.Holder会把摄像头数据拿过来，显示在SurfaceView上面
+                Camera.Parameters params = camera.getParameters();  //Camera对象需要一些参数，我们把它的参数对象拿过来
+                params.setPreviewSize(352, 288);    //将它的参数对象设置一些值，比如说352*288的大小，比如说1920*1080的大不等
+                camera.setParameters(params);   //把参数设置好了后，传回给Camera
+                camera.startPreview();  //开始预览，意思就是开始显示在SurfaceView上面了
+                camera.setDisplayOrientation(90);  //旋转90度，如果不调用这个，会发现报像头是横着的，你可以试下注释掉这一行
+                //camera.setPreviewCallback(this);  //可以给摄像头设置一个回调对象，报像头每采集一些数据，都会回调回来，1秒钟可能采集几十次数据，会回调几十次，这个数据用来计算心率血压等值的
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * SurfaceView开始创建了
+     * @param holder
+     */
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Log.i(TAG, "surfaceCreated..");
@@ -92,6 +96,13 @@ public class RapidExamineFragment extends Fragment implements SurfaceHolder.Call
         }*/
     }
 
+    /**
+     * SurfaceView大小改变了，也就是创建完了
+     * @param holder
+     * @param format
+     * @param width
+     * @param height
+     */
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             //camera.startPreview();
@@ -99,6 +110,10 @@ public class RapidExamineFragment extends Fragment implements SurfaceHolder.Call
         //startPreview();
     }
 
+    /**
+     * SurfaceView要销毁了，你要释放资源，不然camera会一直在内存里面，导致你的内存越用越小
+     * @param holder
+     */
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         Log.i(TAG, "surfaceDestroyed..");
