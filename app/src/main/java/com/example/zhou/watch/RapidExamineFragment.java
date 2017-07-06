27c1aa7b346a9fb75f2e3f3bbabdbfb4c3ea645a
baoer <584515562@qq.com>
2017-07-06 10:11:49 +0800

    增加了打开摄像头功能

--- app/src/main/java/com/example/zhou/watch/RapidExamineFragment.java
+++ app/src/main/java/com/example/zhou/watch/RapidExamineFragment.java
@@ -1,12 +1,20 @@
 package com.example.zhou.watch;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by zhou on 2017/7/5
@@ -15,10 +23,19 @@ import android.view.ViewGroup;
 public class RapidExamineFragment extends Fragment {
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.rapid_examine, container, false);
+        TextView startText = (TextView) view.findViewById(R.id.begin_text);
+        startText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
+
+
