a533c4f686f5ae8010cd622198839bb515a8cd82
jxr202 <jxr202@foxmail.com>
2018-04-10 10:17:30 +0800

    【Jiang】：修复Bug

---
+++ app/src/main/java/com/example/zhou/watch/Uitil/Test.java
@@ -0,0 +1,13 @@
+package com.example.zhou.watch.Uitil;
+
+/**
+ * Created by jxr20 on 2017/7/26
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+
+}
